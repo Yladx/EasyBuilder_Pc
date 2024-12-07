f2b23cbf0f1bc67d51f49e24a218deb739f0ac6e
Yladx <yladd.x@gmail.com>
2024-12-07 21:30:52 +0800

    fix: Update NoHeaderLayout component view path and add storage public files

--- app/View/Components/NoHeaderLayout.php
+++ app/View/Components/NoHeaderLayout.php
@@ -12,6 +12,6 @@ class NoheaderLayout extends Component
      */
     public function render(): View
     {
-        return view('layouts.noheader');
+        return view('components.noheader-layout');
     }
 }
