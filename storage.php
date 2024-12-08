765d433f9513880b2a65bca76e22cbcdad558353
Yladx <yladd.x@gmail.com>
2024-12-08 18:03:56 +0800

    Update storage.php to use copy instead of symlink for better compatibility

--- storage.php
+++ storage.php
@@ -12,12 +12,48 @@ if (file_exists($linkFolder)) {
     }
 }
 
-// Create directory junction (Windows equivalent of symlink)
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    exec('mklink /J "' . str_replace('/', '\\', $linkFolder) . '" "' . str_replace('/', '\\', $targetFolder) . '"');
-} else {
-    symlink($targetFolder, $linkFolder);
+function recursiveCopy($src, $dst) {
+    $dir = opendir($src);
+    if (!file_exists($dst)) {
+        mkdir($dst, 0777, true);
+    }
+    
+    while (($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file)) {
+                recursiveCopy($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                copy($src . '/' . $file, $dst . '/' . $file);
+            }
+        }
+    }
+    closedir($dir);
 }
 
-echo "Storage link created successfully!";
+// Remove existing directory if it exists
+if (file_exists($linkFolder)) {
+    if (is_dir($linkFolder)) {
+        // Remove directory and its contents
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($linkFolder, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+        
+        foreach ($files as $fileinfo) {
+            if ($fileinfo->isDir()) {
+                rmdir($fileinfo->getRealPath());
+            } else {
+                unlink($fileinfo->getRealPath());
+            }
+        }
+        rmdir($linkFolder);
+    } else {
+        unlink($linkFolder);
+    }
+}
+
+// Create directory and copy contents
+recursiveCopy($targetFolder, $linkFolder);
+
+echo "Storage files copied successfully! Your public storage is now accessible.";
 ?>
