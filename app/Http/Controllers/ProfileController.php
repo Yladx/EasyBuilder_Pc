38620cd08d05e31bab4b8f4a08ea1dc3687e1c64
Yladx <yladd.x@gmail.com>
2024-12-17 09:34:36 +0800

    uo

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\View\View;
 use Illuminate\Support\Facades\DB;
 use App\Models\Build;
 
